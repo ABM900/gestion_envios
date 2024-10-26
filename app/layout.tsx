9d76b3794ab92ce069b87bcc7cd1d78aa19bfb76
ABM900 <alv_aro11494@hotmail.com>
2024-10-26 13:31:15 +0200

    Subida inicial

---
+++ app/layout.tsx
@@ -0,0 +1,20 @@
+import './globals.css';
+
+import { Analytics } from '@vercel/analytics/react';
+
+export const metadata = {
+  title: 'Registro envíos'
+};
+
+export default function RootLayout({
+  children
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body className="flex min-h-screen w-full flex-col">{children}</body>
+      <Analytics />
+    </html>
+  );
+}
