fd750443510945295b271f03e00727e8162ded10
ABM900 <alv_aro11494@hotmail.com>
2024-10-27 19:38:58 +0100

    Cambio de ficheros

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -1,20 +1,71 @@
-import './globals.css';
+import Link from 'next/link';
+import {
+    Home,
+    PanelLeft} from 'lucide-react';
 
+import { Button } from '@/components/ui/button';
+import { Sheet, SheetContent, SheetTrigger } from '@/components/ui/sheet';
 import { Analytics } from '@vercel/analytics/react';
+import Providers from './providers';
+import { NavItem } from './nav-item';
+import { SearchInput } from './search';
 
-export const metadata = {
-  title: 'Registro envíos'
-};
-
-export default function RootLayout({
-  children
+export default function DashboardLayout({
+    children
 }: {
-  children: React.ReactNode;
+    children: React.ReactNode;
 }) {
-  return (
-    <html lang="en">
-      <body className="flex min-h-screen w-full flex-col">{children}</body>
-      <Analytics />
-    </html>
-  );
+    return (
+        <Providers>
+            <main className="flex min-h-screen w-full flex-col bg-muted/40">
+                <DesktopNav />
+                <div className="flex flex-col sm:gap-4 sm:py-4 sm:pl-14">
+                    <header className="sticky top-0 z-30 flex h-14 items-center gap-4 border-b bg-background px-4 sm:static sm:h-auto sm:border-0 sm:bg-transparent sm:px-6">
+                        <MobileNav />
+                        <SearchInput />
+                    </header>
+                    <main className="grid flex-1 items-start gap-2 p-4 sm:px-6 sm:py-0 md:gap-4 bg-muted/40">
+                        {children}
+                    </main>
+                </div>
+                <Analytics />
+            </main>
+        </Providers>
+    );
+}
+
+function DesktopNav() {
+    return (
+        <aside className="fixed inset-y-0 left-0 z-10 hidden w-14 flex-col border-r bg-background sm:flex">
+            <nav className="flex flex-col items-center gap-4 px-2 sm:py-5">
+                <NavItem href="#" label="Dashboard">
+                    <Home className="h-5 w-5" />
+                </NavItem>
+            </nav>
+        </aside>
+    );
+}
+
+function MobileNav() {
+    return (
+        <Sheet>
+            <SheetTrigger asChild>
+                <Button size="icon" variant="outline" className="sm:hidden">
+                    <PanelLeft className="h-5 w-5" />
+                    <span className="sr-only">Toggle Menu</span>
+                </Button>
+            </SheetTrigger>
+            <SheetContent side="left" className="sm:max-w-xs">
+                <nav className="grid gap-6 text-lg font-medium">
+                    <Link
+                        href="#"
+                        className="flex items-center gap-4 px-2.5 text-muted-foreground hover:text-foreground"
+                    >
+                        <Home className="h-5 w-5" />
+                        Dashboard
+                    </Link>
+                </nav>
+            </SheetContent>
+        </Sheet>
+    );
 }
