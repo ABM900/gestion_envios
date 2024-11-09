3eeca40ec2e00a8d47b93de1c24fb2e246fc9403
ABM900 <alv_aro11494@hotmail.com>
2024-11-09 22:04:46 +0100

    cosas que no se utilizan

--- app/(dashboard)/addShipping.tsx
+++ app/(dashboard)/addShipping.tsx
@@ -3,8 +3,6 @@
 import { zodResolver } from "@hookform/resolvers/zod"
 import { useForm } from "react-hook-form"
 import { z } from "zod"
-
-import { useToast } from "@/hooks/use-toast";
 import { Button } from "@/components/ui/button"
 import {
     Form,
@@ -38,8 +36,6 @@ type AddShippingFormProps = {
 }
 
 export function AddShippingForm({ onPostAction }: AddShippingFormProps) {
-    const { toast } = useToast();
-
     const form = useForm<z.infer<typeof FormSchema>>({
         resolver: zodResolver(FormSchema),
         defaultValues: {
